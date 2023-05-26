096df103ffb656d05aa04f89f97e0b91045d3fef
masudfcs1 <masudfcs1@gmail.com>
2023-05-26 12:51:45 +0600

    Dune

---
+++ store/cartSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "Cart",
+  initialState: "Hello",
+  reducers: {},
+});
+
+export default cartSlice.reducer;
